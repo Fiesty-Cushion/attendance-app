33cf59969009662e8da8fcaf611c1070acb130fd
Fiesty-Cushion <neupane.braz07@gmail.com>
2023-01-11 15:27:54 +0545

    functional snackbar

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:ui_test/utils/screen_routes.dart';
+import 'package:ui_test/widgets/snackbar.dart';
+
+import '../resources/Authentication/login_fetch.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -9,94 +12,167 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  late Object loginInfo;
+  bool loggedIn = true;
   bool isLoading = false;
+  TextEditingController usernameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    usernameController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      color: Colors.white,
-      child: SingleChildScrollView(
-        child: SafeArea(
-          child: Column(
-            children: [
-              Image.asset(
-                "assets/images/welcome.png",
-                fit: BoxFit.cover,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                    vertical: 10.0, horizontal: 20.0),
-                child: Column(
-                  children: [
-                    TextFormField(
-                      decoration: const InputDecoration(
-                        hintText: "Username",
-                        label: Text("Enter Username"),
+    return Scaffold(
+      body: GestureDetector(
+        onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+        child: Material(
+          color: Colors.white,
+          child: SingleChildScrollView(
+            child: SafeArea(
+              child: Column(
+                children: [
+                  Image.asset(
+                    "assets/images/welcome.png",
+                    fit: BoxFit.cover,
+                  ),
+                  Form(
+                    key: _formKey,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 10.0, horizontal: 20.0),
+                      child: Column(
+                        children: [
+                          const SizedBox(height: 20),
+                          TextFormField(
+                            autovalidateMode:
+                            AutovalidateMode.onUserInteraction,
+                            onTap: () {
+                              setState(() {});
+                            },
+                            textInputAction: TextInputAction.next,
+                            controller: usernameController,
+                            decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: 'Enter Username',
+                            ),
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return "Username can't be empty";
+                              }
+                              return null;
+                            },
+                          ),
+                          const SizedBox(height: 30),
+                          TextFormField(
+                            autovalidateMode:
+                            AutovalidateMode.onUserInteraction,
+                            onTap: () {
+                              setState(() {});
+                            },
+                            textInputAction: TextInputAction.done,
+                            obscureText: true,
+                            controller: passwordController,
+                            decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: 'Enter Password',
+                            ),
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return "Password can't be empty";
+                              }
+                              return null;
+                            },
+                          )
+                        ],
                       ),
-                    ),
-                    TextFormField(
-                      obscureText: true,
-                      decoration: const InputDecoration(
-                        hintText: "Password",
-                        label: Text("Enter Password"),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(height: 20.0),
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                    vertical: 20.0, horizontal: 60.0),
-                child: ElevatedButton(
-                  onPressed: () async {
-                    if (isLoading) return;
-                    setState(() {
-                      isLoading = true;
-                    });
-                    await Future.delayed(Duration(seconds: 2));
-
-                    setState(() {
-                      Navigator.pushNamed(context, MyRoute.mainRoute);
-                      isLoading = false;
-                    });
-                  },
-                  child: isLoading
-                      ? CircularProgressIndicator(
-                          color: Colors.white,
-                          strokeWidth: 2.0,
-                        )
-                      : Text(
-                          "Login",
-                          style: TextStyle(color: Colors.white),
-                        ),
-                  style: ElevatedButton.styleFrom(
-                      textStyle: const TextStyle(
-                        fontSize: 17.0,
-                        fontWeight: FontWeight.bold,
-                      ),
-                      shape: StadiumBorder(),
-                      minimumSize: Size.fromHeight(60)),
-                ),
-              ),
-              TextButton(
-                  onPressed: () {
-                    setState(() {
-                      Navigator.pushNamed(context, MyRoute.registerRoute);
-                    });
-                  },
-                  child: Text(
-                      "Register",
-                    style: TextStyle(
-                      color: Colors.grey[700],
-                      decoration: TextDecoration.underline,
                     ),
                   ),
+                  const SizedBox(height: 20.0),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 20.0, horizontal: 60.0),
+                    child: ElevatedButton(
+                      onPressed: () async {
+                        if (_formKey.currentState!.validate()) {
+                          if (isLoading) return;
+                          setState(() {
+                            isLoading = true;
+                          });
+
+                          var loginInfo = await fetchLogin(
+                              usernameController.text.toString(),
+                              passwordController.text.toString());
+
+                          if (!mounted) return;
+
+                          if (loginInfo.accessToken != null) {
+                            setState(() {
+                              loggedIn = true;
+                            });
+                          } else if (loginInfo.errorDescription != null) {
+                            ScaffoldMessenger.of(context)
+                                .showSnackBar(buildSnackBar(loginInfo.errorDescription));
+                          }
+
+                          if (loggedIn) {
+                            setState(() {
+                              Navigator.pushNamed(context, MyRoute.mainRoute);
+                              isLoading = false;
+                              loggedIn = false;
+                            });
+                          } else {
+                            setState(() {
+                              isLoading = false;
+                            });
+                          }
+                        }
+                      },
+                      style: ElevatedButton.styleFrom(
+                          textStyle: const TextStyle(
+                            fontSize: 17.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          shape: const StadiumBorder(),
+                          minimumSize: const Size.fromHeight(60)),
+                      child: isLoading
+                          ? const CircularProgressIndicator(
+                        color: Colors.white,
+                        strokeWidth: 2.0,
+                      )
+                          : const Text(
+                        "Login",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      setState(() {
+                        Navigator.pushNamed(context, MyRoute.registerRoute);
+                      });
+                    },
+                    child: Text(
+                      "Register",
+                      style: TextStyle(
+                        color: Colors.grey[700],
+                        decoration: TextDecoration.underline,
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
         ),
       ),
     );
   }
+
 }
